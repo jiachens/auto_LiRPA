4da103c1efa23d93d87236ff57dc1364311a75f1
Jiachen Sun <jcsun0806@gmail.com>
2022-01-26 23:50:33 -0500

    fxi bug of resnet18 in cifar training

--- examples/vision/cifar_training.py
+++ examples/vision/cifar_training.py
@@ -208,7 +208,7 @@ def main(args):
         model_ori = models.Models[args.model](in_ch=1, in_dim=28)
     else:
         if args.model == 'resnet18':
-            model_ori = models.Models[args.model]
+            model_ori = models.Models[args.model]()
         else:
             model_ori = models.Models[args.model](in_ch=3, in_dim=32)
     epoch = 0
